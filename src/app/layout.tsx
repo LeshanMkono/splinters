f8e5ebac8e49098f3a79d3ba5344bb7c6237b5ff
Giles Leshan <mkonodigital@gmail.com>
2026-04-06 15:52:32 +0300

    Add Google Analytics 4 GA4 tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import Script from "next/script";
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://splinters.co.ke"),
   title: {
@@ -51,6 +53,20 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FWW054P0X0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FWW054P0X0');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   );
